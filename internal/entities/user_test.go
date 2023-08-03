0c82aaa934d59cddadf4bcf360b6fbd014a296f5
Carlos Ferreira <carlos@especializati.com.br>
2023-08-03 11:31:49 -0300

    test: create new user

---
+++ internal/entities/user_test.go
@@ -0,0 +1,17 @@
+package entities
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewUser(t *testing.T) {
+	user, err := NewUser("Carlos", "carlos@especializati.com.br", "123456")
+	assert.Nil(t, err)
+	assert.NotNil(t, user)
+	assert.NotEmpty(t, user.ID)
+	assert.NotEmpty(t, user.Password)
+	assert.Equal(t, "Carlos", user.Name)
+	assert.Equal(t, "carlos@especializati.com.br", user.Email)
+}
