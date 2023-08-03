76ef2ac7466ef7cd66116801ee130c3f7e7676e2
Carlos Ferreira <carlos@especializati.com.br>
2023-08-03 11:21:47 -0300

    feat: create value object uuid

---
+++ pkg/value_object/id.go
@@ -0,0 +1,14 @@
+package value_object
+
+import "github.com/google/uuid"
+
+type ID = uuid.UUID
+
+func NewId() ID {
+	return ID(uuid.New())
+}
+
+func ParseID(s string) (ID, error) {
+	id, err := uuid.Parse(s)
+	return ID(id), err
+}
