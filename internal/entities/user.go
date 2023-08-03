93d7c7420659ee8b53e841ebfbd4d1101b691a61
Carlos Ferreira <carlos@especializati.com.br>
2023-08-03 11:16:04 -0300

    feat: func create new user and encrypt password

--- internal/entities/user.go
+++ internal/entities/user.go
@@ -1,8 +1,25 @@
 package entities
 
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
 type User struct {
 	ID       string `json:"id"`
 	Name     string `json:"name"`
 	Email    string `json:"email"`
 	Password string `json:"-"`
+}
+
+func NewUser(name, email, password string) (*User, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, err
+	}
+	return &User{
+		ID:       "id",
+		Name:     name,
+		Email:    email,
+		Password: string(hash),
+	}, nil
 }
