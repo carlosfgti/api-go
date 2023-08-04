572635061c4a718e786b2a83afdf19e674c490af
Carlos Ferreira <carlos@especializati.com.br>
2023-08-04 11:09:18 -0300

    test: test create new user

---
+++ internal/infra/database/user_db_test.go
@@ -0,0 +1,31 @@
+package database
+
+import (
+	"testing"
+
+	"github.com/carlosfgti/go-api/internal/entities"
+	"github.com/stretchr/testify/assert"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func TestCreateUser(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open("file:memory:"), &gorm.Config{})
+	if err != nil {
+		t.Error(err)
+	}
+	db.AutoMigrate(entities.User{})
+	user, _ := entities.NewUser("Carlos", "carlos@especializati.com.br", "123456")
+	userDB := NewUser(db)
+
+	err = userDB.Create(user)
+	assert.Nil(t, err)
+
+	var userFound entities.User
+	err = db.First(&userFound, "id = ?", user.ID).Error
+	assert.Nil(t, err)
+	assert.Equal(t, user.ID, userFound.ID)
+	assert.Equal(t, user.Name, userFound.Name)
+	assert.Equal(t, user.Email, userFound.Email)
+	assert.NotNil(t, userFound.Password)
+}
