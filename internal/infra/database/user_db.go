9ffd687d164c6ce3a02ff803c84f15117e105e32
Carlos Ferreira <carlos@especializati.com.br>
2023-08-07 15:06:40 -0300

    feat: get user by email

--- internal/infra/database/user_db.go
+++ internal/infra/database/user_db.go
@@ -16,3 +16,11 @@ func NewUser(db *gorm.DB) *User {
 func (u *User) Create(user *entities.User) error {
 	return u.DB.Create(user).Error
 }
+
+func (u *User) FindByEmail(email string) (*entities.User, error) {
+	var user entities.User
+	if err := u.DB.Where("email = ?", email).First(&user).Error; err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
