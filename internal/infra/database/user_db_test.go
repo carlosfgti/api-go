2cc0b79c0a74c37362e31f7c33bd6a0f58f65eac
Carlos Ferreira <carlos@especializati.com.br>
2023-08-07 15:07:39 -0300

    test: test method find user by email

--- internal/infra/database/user_db_test.go
+++ internal/infra/database/user_db_test.go
@@ -29,3 +29,22 @@ func TestCreateUser(t *testing.T) {
 	assert.Equal(t, user.Email, userFound.Email)
 	assert.NotNil(t, userFound.Password)
 }
+
+func TestFindByEmail(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open("file:memory:"), &gorm.Config{})
+	if err != nil {
+		t.Error(err)
+	}
+	db.AutoMigrate(entities.User{})
+	user, _ := entities.NewUser("Carlos", "carlos@especializati.com.br", "123456")
+	userDB := NewUser(db)
+
+	err = userDB.Create(user)
+	assert.Nil(t, err)
+
+	userFound, err := userDB.FindByEmail(user.Email)
+	assert.Equal(t, user.ID, userFound.ID)
+	assert.Equal(t, user.Name, userFound.Name)
+	assert.Equal(t, user.Email, userFound.Email)
+	assert.NotNil(t, userFound.Password)
+}
